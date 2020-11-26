230df062214efe99b882bbb3dc15c14084b7f096
Robbie Thandi <robbie.thandi@gmail.com>
2020-11-26 23:56:58 +0000

    Maybe finished wc

--- wc.py
+++ wc.py
@@ -1,8 +1,9 @@
 import argparse
+import sys
 
-def outputBuilder(lines, words, chars, maxLength, lPresent, wPresent, cPresent, LPresent, fileName):
+def outputBuilder(lines, words, chars, mCount, maxLength, lPresent, wPresent, cPresent, mPresent, LPresent, fileName):
     outputString = "\t"
-    if not lPresent and not wPresent and not cPresent and not LPresent:
+    if not lPresent and not wPresent and not cPresent and not LPresent and not mPresent:
         outputString += lines + "\t" + words + "\t" + chars + "\t"
     else:
         if lPresent:
@@ -11,6 +12,8 @@ def outputBuilder(lines, words, chars, maxLength, lPresent, wPresent, cPresent, 
             outputString += words + "\t"
         if cPresent:
             outputString += chars + "\t"
+        if mPresent:
+            outputString += mCount + "\t"
         if LPresent:
             outputString += maxLength + "\t"
     output = outputString + fileName
@@ -21,29 +24,30 @@ def processInput():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument("filename", type=str, action="append", nargs='*')
     parser.add_argument("-l", action="store_true")
-    parser.add_argument("-lines", action="store_true")
+    parser.add_argument("--lines", action="store_true")
     parser.add_argument("-w", action="store_true")
-    parser.add_argument("-words", action="store_true")
+    parser.add_argument("--words", action="store_true")
     parser.add_argument("-c", action="store_true")
-    parser.add_argument("-chars", action="store_true")
+    parser.add_argument("--bytes", action="store_true")
+    parser.add_argument("-m", action="store_true")
+    parser.add_argument("--chars", action="store_true")
     parser.add_argument("-L", action="store_true")
     parser.add_argument("--max-line-length", action="store_true")
     parser.add_argument("--files0-from", action="store")
     return parser.parse_known_intermixed_args()
 
 def verifyInputs(knownArgs):
-    validInputs = ["--bytes", "-m", "--chars", "--lines", "--words", "--"]
     if knownArgs:
         for i in range(0, len(knownArgs)):
             currentArg = knownArgs[i]
-            if currentArg == "--help" or currentArg == "-h":
+            if currentArg == "--help":
                 printHelp()
                 return "help"
             elif currentArg == "--version":
                 printVersion()
                 return "version"
-            elif currentArg in validInputs:
-                return notHandled()
+            elif currentArg == "--":
+                processFiles(args, justDash=True)
             else:
                 print("wc: invalid option -- '" + currentArg[1] + "'")
                 print("Try 'python3 wc --help' for more information.")
@@ -52,7 +56,12 @@ def verifyInputs(knownArgs):
         return True
 
 def processFlags(args):
-    return args.l, args.w, args.c, args.L, args.max_line_length
+    lPresent = args.l or args.lines
+    wPresent = args.w or args.words
+    cPresent = args.c or args.bytes
+    mPresent = args.m or args.chars
+    maxLengthPresent = args.L or args.max_line_length
+    return lPresent, wPresent, cPresent, mPresent, maxLengthPresent
 
 def printHelp():
     print(
@@ -102,14 +111,16 @@ def filesFrom(input):
         print('wc: cannot open ' + input + ' for reading: No such file or directory')
         return "error"
 
-def processFiles(args):
+def processFiles(args, justDash=False):
     # Process arguments
-    lPresent, wPresent, cPresent, LPresent, maxLineLength = processFlags(args)
-    maxLineLengthPresent = LPresent or maxLineLength
-    # hPresent = hPresent or helpPresent
+    lPresent, wPresent, cPresent, mPresent, maxLineLengthPresent = processFlags(args)
     filesFromInput = args.files0_from
 
     fileInputs = args.filename[0]
+    if justDash or len(fileInputs) == 0:
+        charCount, wordCount, lineCount, mCount, maxLength = stdinRead()
+        outputBuilder(str(lineCount), str(wordCount), str(charCount), str(mCount), str(maxLength), lPresent, wPresent, cPresent, mPresent, maxLineLengthPresent, '')
+        return "empty"
 
     if filesFromInput and len(fileInputs) > 0:
         print('wc: extra operand ' + fileInputs[0])
@@ -125,53 +136,70 @@ def processFiles(args):
     totalL = 0
     totalW = 0
     totalC = 0
+    totalM = 0
     totalMaxLength = 0
 
     isDir = False
     notFound = False
 
-    if len(fileInputs) > 0:
-        for i in range(0, len(fileInputs)):
-            userInput = fileInputs[i]
-            if userInput == "-":
-                return notHandled()
+    charCount = wordCount = lineCount = mCount = maxLength = None
+    for i in range(0, len(fileInputs)):
+        userInput = fileInputs[i]
+        if userInput == '' or userInput == "":
+            print("wc: invalid zero-length file name")
+            charCount = wordCount = lineCount = mCount = maxLength = 0
+            if len(fileInputs) <= 1:
+                return "zero length"
+        elif userInput == "-":
+            charCount, wordCount, lineCount, mCount, maxLength = stdinRead()
+        else:
             try:
                 file = open(userInput, mode='r+b')
                 fileRead = file.read()
                 charCount = len(fileRead)
                 wordCount = len(fileRead.split())
                 lineCount = len(fileRead.split('\n'.encode())) - 1
+                mCount =  len(fileRead.decode('utf-8', errors='ignore'))
                 maxLength = len(max(open(userInput, mode='r', encoding="latin1"), key=len)) - 1
                 # If the last character is a new line we need to add another line on
                 if charCount > 0 and fileRead[-1] == '\n'.encode():
                     lineCount += 1
-                outputBuilder(str(lineCount), str(wordCount), str(charCount), str(maxLength), lPresent, wPresent, cPresent, maxLineLengthPresent, userInput)
-                totalC += charCount
-                totalW += wordCount
-                totalL += lineCount
-                #update longest line
-                if maxLength > totalMaxLength:
-                    totalMaxLength = maxLength
                 file.close()
             except FileNotFoundError:
                 print("wc: " + userInput + ": No such file or directory")
                 notFound = True
             except IsADirectoryError:
                 print("wc: " + userInput + ": Is a directory")
-                outputBuilder("0", "0", "0", "0", lPresent, wPresent, cPresent, maxLineLengthPresent, userInput)
-        if len(fileInputs) > 1:
-            outputBuilder(str(totalL), str(totalW), str(totalC), str(totalMaxLength), lPresent, wPresent, cPresent, maxLineLengthPresent, "total")
-        return totalL, totalW, totalC, isDir, notFound
-    else:
-        # This would usually allow stdin until ctrl-d is pressed when it will execute with that input
-        return notHandled()
+                outputBuilder("0", "0", "0", "0", "0", lPresent, wPresent, cPresent, mPresent, maxLineLengthPresent, userInput)
+        outputBuilder(str(lineCount), str(wordCount), str(charCount), str(mCount), str(maxLength), lPresent, wPresent, cPresent, mPresent, maxLineLengthPresent, userInput)
+        totalC += charCount
+        totalW += wordCount
+        totalL += lineCount
+        totalM += mCount
+        #update longest line
+        if maxLength > totalMaxLength:
+            totalMaxLength = maxLength
+    if len(fileInputs) > 1:
+        outputBuilder(str(totalL), str(totalW), str(totalC), str(totalM), str(totalMaxLength), lPresent, wPresent, cPresent, mPresent, maxLineLengthPresent, "total")
+    return totalL, totalW, totalC, isDir, notFound
+
+def stdinRead():
+    input = sys.stdin.readlines()
+    input = '\n'.join(input)
+    charCount = mCount = len(input)
+    wordCount = len(input.split())
+    lineCount = len(input.split('\n')) - 1
+    maxLength = len(max(input, key=len))
+    # If the last character is a new line we need to add another line on
+    if charCount > 0 and input[-1] == '\n'.encode():
+        lineCount += 1
+    return charCount, wordCount, lineCount, mCount, maxLength
 
 def notHandled():
     print("We don't handle that situation yet!")
     return "not handled"
 
 def run(args, knownArgs):
-    print(knownArgs)
     verif = verifyInputs(knownArgs)
     if verif and verif != 'invalid' and verif != 'not handled' and verif != 'help' and verif != 'version':
         return processFiles(args)
